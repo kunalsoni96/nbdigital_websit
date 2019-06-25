0fd6f604a879e6d50eeabad333e1b62da4759cce
kunal <kunal1071996@gmail.com>
2019-06-25 13:00:27 +0530

    first comment

---
+++ index.php
@@ -0,0 +1,503 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="author" content="Grayrids">
+<title>One Second Landing Template</title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/line-icons.css">
+<link rel="stylesheet" href="css/nivo-lightbox.css">
+<link rel="stylesheet" href="css/magnific-popup.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/responsive.css">
+</head>
+<body>
+
+
+<div id="subscribe" class="section">
+<div class="container">
+<div class="row justify-content-between">
+<div class="col-lg-5 col-md-12 col-xs-12">
+<div class="subscribe-form">
+<div class="form-wrapper">
+<div class="sub-title text-center">
+<h3>Connect Us</h3>
+<p>Weekly Freebies and More!</p>
+</div>
+<form>
+<div class="row">
+<div class="col-12 form-line">
+<div class="form-group">
+<input type="text" class="form-control" name="email" placeholder="Name">
+</div>
+</div>
+<div class="col-md-12 form-line">
+<div class="form-group">
+<input type="text" class="form-control" name="phone" placeholder="Phone">
+</div>
+</div>
+<div class="col-12 form-line">
+<div class="form-group">
+<input type="email" class="form-control" name="email" placeholder="Email">
+</div>
+</div>
+<div class="col-12">
+<div class="form-submit">
+<button type="submit" class="btn btn-common btn-effect">Submit Now</button>
+</div>
+</div>
+</div>
+</form>
+</div>
+</div>
+</div>
+<div class="col-lg-6 col-md-12 col-xs-12">
+<div class="sub-item-box">
+<div class="icon-box">
+<i class="lni-bullhorn"></i>
+</div>
+<div class="text-box">
+<h4>Weekly Free Learning Materials</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, laborum libero beatae obcaecati.</p>
+</div>
+</div>
+<div class="sub-item-box">
+<div class="icon-box">
+<i class="lni-book"></i>
+</div>
+<div class="text-box">
+<h4>Free PDF to Get Started</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, laborum libero beatae obcaecati.</p>
+</div>
+</div>
+<div class="sub-item-box">
+<div class="icon-box">
+<i class="lni-timer"></i>
+</div>
+<div class="text-box">
+<h4>10% Instant Discount</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, laborum libero beatae obcaecati.</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<section id="services" class="section">
+<div class="container">
+<div class="section-header">
+<h2 class="section-title">Our Services</h2>
+<span>Services</span>
+<p class="section-subtitle">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy</p>
+</div>
+<div class="row">
+<div class="col-lg-4 col-md-6 col-xs-12">
+<div class="item-boxes services-item wow fadeInDown" data-wow-delay="0.2s">
+<div class="icon color-1">
+<i class="lni-pencil"></i>
+</div>
+<h4>Content Writing</h4>
+<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-xs-12">
+<div class="item-boxes services-item wow fadeInDown" data-wow-delay="0.4s">
+<div class="icon color-2">
+<i class="lni-cog"></i>
+</div>
+<h4>Web Development</h4>
+<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-xs-12">
+<div class="item-boxes services-item wow fadeInDown" data-wow-delay="0.6s">
+<div class="icon color-3">
+<i class="lni-stats-up"></i>
+</div>
+<h4>Graphic Design</h4>
+<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-xs-12">
+<div class="item-boxes services-item wow fadeInDown" data-wow-delay="0.8s">
+<div class="icon color-4">
+<i class="lni-layers"></i>
+</div>
+<h4>UI/UX Design</h4>
+<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-xs-12">
+<div class="item-boxes services-item wow fadeInDown" data-wow-delay="1s">
+<div class="icon color-5">
+<i class="lni-tab"></i>
+</div>
+<h4>App Development</h4>
+<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-xs-12">
+<div class="item-boxes services-item wow fadeInDown" data-wow-delay="1.2s">
+<div class="icon color-6">
+<i class="lni-briefcase"></i>
+</div>
+<h4>Digital Marketing</h4>
+<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+
+
+
+<section id="features" class="section">
+<div class="container">
+<div class="section-header">
+<h2 class="section-title">Why Choose Us</h2>
+<span>Why</span>
+<p class="section-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos debitis.</p>
+</div>
+<div class="row">
+
+<div class="col-lg-4 col-md-6 col-xs-12">
+<div class="featured-box">
+<div class="featured-icon">
+<i class="lni-layout"></i>
+</div>
+<div class="featured-content">
+<div class="icon-o"><i class="lni-layout"></i></div>
+<h4>Refreshing Design</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et magna aliqua.</p>
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-4 col-md-6 col-xs-12">
+<div class="featured-box">
+<div class="featured-icon">
+<i class="lni-tab"></i>
+</div>
+<div class="featured-content">
+<div class="icon-o"><i class="lni-tab"></i></div>
+<h4>Fully Responsive</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et magna aliqua.</p>
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-4 col-md-6 col-xs-12">
+<div class="featured-box">
+<div class="featured-icon">
+<i class="lni-rocket"></i>
+</div>
+<div class="featured-content">
+<div class="icon-o"><i class="lni-rocket"></i></div>
+<h4>Fast & Smooth</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et magna aliqua.</p>
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-4 col-md-6 col-xs-12">
+<div class="featured-box">
+<div class="featured-icon">
+<i class="lni-database"></i>
+</div>
+<div class="featured-content">
+<div class="icon-o"><i class="lni-database"></i></div>
+<h4>SEO Optimized</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et magna aliqua.</p>
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-4 col-md-6 col-xs-12">
+<div class="featured-box">
+<div class="featured-icon">
+<i class="lni-leaf"></i>
+</div>
+<div class="featured-content">
+<div class="icon-o"><i class="lni-leaf"></i></div>
+<h4>Clean Code</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et magna aliqua.</p>
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-4 col-md-6 col-xs-12">
+<div class="featured-box">
+<div class="featured-icon">
+<i class="lni-pencil"></i>
+</div>
+<div class="featured-content">
+<div class="icon-o"><i class="lni-pencil"></i></div>
+<h4>Free 24/7 Support</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et magna aliqua.</p>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+</section>
+
+
+<div id="pricing" class="section pricing-section">
+<div class="container">
+<div class="section-header">
+<h2 class="section-title">Pricing Plans</h2>
+<span>Pricing</span>
+<p class="section-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos debitis.</p>
+</div>
+<div class="row pricing-tables">
+<div class="col-lg-4 col-md-4 col-xs-12">
+<div class="pricing-table">
+<div class="pricing-details">
+<h2>Starter Plan</h2>
+<div class="price">49$ <span>/mo</span></div>
+<ul>
+<li>Consectetur adipiscing</li>
+<li>Nunc luctus nulla et tellus</li>
+<li>Suspendisse quis metus</li>
+<li>Vestibul varius fermentum erat</li>
+<li> - </li>
+</ul>
+</div>
+<div class="plan-button">
+<a href="#" class="btn btn-common btn-effect">Get Plan</a>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-4 col-xs-12">
+<div class="pricing-table pricing-big">
+<div class="pricing-details">
+<h2>Popular Plan</h2>
+<div class="price">99$ <span>/mo</span></div>
+<ul>
+<li>Consectetur adipiscing</li>
+<li>Nunc luctus nulla et tellus</li>
+<li>Suspendisse quis metus</li>
+<li>Vestibul varius fermentum erat</li>
+<li> - </li>
+</ul>
+</div>
+<div class="plan-button">
+<a href="#" class="btn btn-common btn-effect">Buy Now</a>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-4 col-xs-12">
+<div class="pricing-table">
+<div class="pricing-details">
+<h2>Premium Plan</h2>
+<div class="price">199$ <span>/mo</span></div>
+<ul>
+<li>Consectetur adipiscing</li>
+<li>Nunc luctus nulla et tellus</li>
+<li>Suspendisse quis metus</li>
+<li>Vestibul varius fermentum erat</li>
+<li>Suspendisse quis metus</li>
+</ul>
+</div>
+<div class="plan-button">
+<a href="#" class="btn btn-common btn-effect">Buy Now</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<section id="team" class="section">
+<div class="container">
+<div class="section-header">
+<h2 class="section-title">Our Team</h2>
+<span>Team</span>
+<p class="section-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos debitis.</p>
+</div>
+<div class="row">
+<div class="col-lg-3 col-md-6 col-xs-12">
+<div class="single-team">
+<img src="img/team/team1.png" alt="">
+<div class="team-details">
+<div class="team-inner">
+<h4 class="team-title">Jhon Doe</h4>
+<p>Chief Technical Officer</p>
+<ul class="social-list">
+<li class="facebook"><a href="#"><i class="lni-facebook-filled"></i></a></li>
+<li class="twitter"><a href="#"><i class="lni-twitter-filled"></i></a></li>
+<li class="google-plus"><a href="#"><i class="lni-google-plus"></i></a></li>
+<li class="linkedin"><a href="#"><i class="lni-linkedin-fill"></i></a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-md-6 col-xs-12">
+<div class="single-team">
+<img src="img/team/team2.png" alt="">
+<div class="team-details">
+<div class="team-inner">
+<h4 class="team-title">Luke Chesser</h4>
+<p>Marketing Executive</p>
+<ul class="social-list">
+<li class="facebook"><a href="#"><i class="lni-facebook-filled"></i></a></li>
+<li class="twitter"><a href="#"><i class="lni-twitter-filled"></i></a></li>
+<li class="google-plus"><a href="#"><i class="lni-google-plus"></i></a></li>
+<li class="linkedin"><a href="#"><i class="lni-linkedin-fill"></i></a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-md-6 col-xs-12">
+<div class="single-team">
+<img src="img/team/team3.png" alt="">
+<div class="team-details">
+<div class="team-inner">
+<h4 class="team-title">David Givens</h4>
+<p>Business Manager</p>
+<ul class="social-list">
+<li class="facebook"><a href="#"><i class="lni-facebook-filled"></i></a></li>
+<li class="twitter"><a href="#"><i class="lni-twitter-filled"></i></a></li>
+<li class="google-plus"><a href="#"><i class="lni-google-plus"></i></a></li>
+<li class="linkedin"><a href="#"><i class="lni-linkedin-fill"></i></a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-md-6 col-xs-12">
+<div class="single-team">
+<img src="img/team/team4.png" alt="">
+<div class="team-details">
+<div class="team-inner">
+<h4 class="team-title">Annie Spratt</h4>
+ <p>Graphic Designer</p>
+<ul class="social-list">
+<li class="facebook"><a href="#"><i class="lni-facebook-filled"></i></a></li>
+<li class="twitter"><a href="#"><i class="lni-twitter-filled"></i></a></li>
+<li class="google-plus"><a href="#"><i class="lni-google-plus"></i></a></li>
+<li class="linkedin"><a href="#"><i class="lni-linkedin-fill"></i></a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+
+
+
+<section id="contact" class="section">
+<div class="contact-form">
+<div class="container">
+<div class="section-header">
+<h2 class="section-title">Get In Touch</h2>
+<span>Contact</span>
+<p class="section-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos debitis.</p>
+</div>
+<div class="row">
+<div class="col-lg-9 col-md-9 col-xs-12">
+<div class="contact-block">
+<form id="contactForm">
+<div class="row">
+<div class="col-md-6">
+<div class="form-group">
+<input type="text" class="form-control" id="name" name="name" placeholder="Your Name" required data-error="Please enter your name">
+<div class="help-block with-errors"></div>
+</div>
+</div>
+<div class="col-md-6">
+<div class="form-group">
+<input type="text" placeholder="Your Email" id="email" class="form-control" name="name" required data-error="Please enter your email">
+<div class="help-block with-errors"></div>
+</div>
+</div>
+<div class="col-md-12">
+<div class="form-group">
+<input type="text" placeholder="Subject" id="msg_subject" class="form-control" required data-error="Please enter your subject">
+<div class="help-block with-errors"></div>
+</div>
+</div>
+<div class="col-md-12">
+<div class="form-group">
+<textarea class="form-control" id="message" placeholder="Your Message" rows="7" data-error="Write your message" required></textarea>
+<div class="help-block with-errors"></div>
+</div>
+<div class="submit-button">
+<button class="btn btn-common btn-effect" id="submit" type="submit">Send Message</button>
+<div id="msgSubmit" class="h3 hidden"></div>
+<div class="clearfix"></div>
+</div>
+</div>
+</div>
+</form>
+</div>
+</div>
+<div class="col-lg-3 col-md-3 col-xs-12">
+<div class="contact-deatils">
+
+<div class="contact-info_area">
+<div class="contact-info">
+<i class="lni-map"></i>
+<h5>Location</h5>
+<p>1900 Pico Blvd, New York br Centernial, colorado</p>
+</div>
+
+<div class="contact-info">
+<i class="lni-star"></i>
+<h5>E-mail</h5>
+<p>info@example.com</p>
+</div>
+
+<div class="contact-info">
+<i class="lni-phone"></i>
+<h5>Phone</h5>
+<p>+91 XXXXXXXXXX</p>
+</div>
+
+<ul class="footer-social">
+<li><a class="facebook" href="#"><i class="lni-facebook-filled"></i></a></li>
+<li><a class="twitter" href="#"><i class="lni-twitter-filled"></i></a></li>
+<li><a class="linkedin" href="#"><i class="lni-linkedin-fill"></i></a></li>
+<li><a class="google-plus" href="#"><i class="lni-google-plus"></i></a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div> 
+</section>
+
+<a href="#" class="back-to-top">
+<i class="lni-arrow-up"></i>
+</a>
+
+<script src="js/jquery-min.js"></script>
+<script src="js/popper.min.js"async></script>
+<script src="js/bootstrap.min.js"async></script>
+<script src="js/jquery.mixitup.js" async></script>
+<script src="js/nivo-lightbox.js" async></script>
+<script src="js/owl.carousel.js" async></script>
+<script src="js/form-validator.min.js" async></script>
+<script src="js/contact-form-script.js" async></script>
+<script src="js/main.js" async></script>
+</body>
+</html>
